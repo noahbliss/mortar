894898c5752c6d57e6248a79ed6695f47932ac44
Noah <noah@superuser.sh>
2020-01-13 17:44:42 -0500

    more rework of 2

--- 2-eficompilesigninstall.sh
+++ 2-eficompilesigninstall.sh
@@ -9,28 +9,30 @@ if [ -z $KERNELFILE ]; then KERNELFILE='/boot/vmlinuz-linux'; fi
 if [ -z $INITRAMFSFILE ]; then INITRAMFSFILE='/boot/initramfs-linux.img'; fi
 
 testexist() {
-	if ! [ -f "$1" ]; then echo "Usage: $0 kernelpath initramfspath"; echo "Could not locate $1"; exit 1; fi
+	if ! [ -f "$2" ]; then echo "Usage: $0 kernelpath initramfspath"; echo "Could not locate $1 at $2"; exit 1; fi
 }
 
-testexist "$CMDLINEFILE"
-testexist "$KERNELFILE"
-testexist "$INITRAMFSFILE"
-testexist "$EFISTUBFILE"
-testexist /etc/os-release
+testexist CMDLINEFILE "$CMDLINEFILE"
+testexist KERNELFILE "$KERNELFILE"
+testexist INITRAMFSFILE "$INITRAMFSFILE"
+testexist EFISTUBFILE "$EFISTUBFILE"
+testexist os-release /etc/os-release
+UNSIGNEDEFIPATH="$EFI_DIR$EFI_NAME.unsigned"
+SIGNEDEFIPATH="$EFI_DIR$EFI_NAME"
 
 objcopy \
     --add-section .osrel=/etc/os-release --change-section-vma .osrel=0x20000 \
     --add-section .cmdline="$CMDLINEFILE" --change-section-vma .cmdline=0x30000 \
     --add-section .linux="$KERNELFILE" --change-section-vma .linux=0x40000 \
     --add-section .initrd="$INITRAMFSFILE" --change-section-vma .initrd=0x3000000 \
-    "$EFISTUBFILE" "$EFI_DIR$EFI_NAME.unsigned"
+    "$EFISTUBFILE" "$UNSIGNEDEFIPATH"
 
-if [ -f "$EFI_DIR$EFI_NAME.unsigned" ]; then echo "Created "$EFI_DIR$EFI_NAME.unsigned"; else echo "Failed to create joined efi file at "$EFI_DIR$EFI_NAME.unsigned"; exit 1; fi
+if [ -f "$UNSIGNEDEFIPATH" ]; then echo "Created "$UNSIGNEDEFIPATH"; else echo "Failed to create joined efi file at "$UNSIGNEDEFIPATH"; exit 1; fi
 echo "Signing..."
 
 # Sign the new file. 
-sbsign --key "$SECUREBOOT_DB_KEY" --cert "$SECUREBOOT_DB_CRT" --output "$EFI_DIR$EFI_NAME" "$EFI_DIR$EFI_NAME.unsigned"
+sbsign --key "$SECUREBOOT_DB_KEY" --cert "$SECUREBOOT_DB_CRT" --output "$SIGNEDEFIPATH" "$UNSIGNEDEFIPATH"
 
-if [ -f "$EFI_DIR$EFI_NAME" ]; then echo "Created signed "$EFI_DIR$EFI_NAME"; else echo "Failed to create signed efi file at "$EFI_DIR$EFI_NAME"; exit 1; fi
+if [ -f "$SIGNEDEFIPATH" ]; then echo "Created signed "$SIGNEDEFIPATH"; else echo "Failed to create signed efi file at "$SIGNEDEFIPATH"; exit 1; fi
 #echo "Removing unsigned efi file..."
-#if (rm "$EFI_DIR$EFI_NAME.unsigned"); then echo "Removed unsigned file."; else echo "Failed to remove unsigned file."; fi
+#if (rm "$UNSIGNEDEFIPATH"); then echo "Removed unsigned file."; else echo "Failed to remove unsigned file."; fi
