b07c7142cee4d8dcaa4aa027f1388e55caf9cedc
Noah <noah@superuser.sh>
2020-01-14 18:39:30 -0500

    initial rework of 4-prepluks.sh

--- 0-initialsetup-prereqinstall.sh
+++ 0-initialsetup-prereqinstall.sh
@@ -40,3 +40,9 @@ if ! [ -f "$ENVFILE" ]; then echo "Generating new KEY_UUID and installing mortar
 # Install cmdline.conf
 if ! [ -f "$CMDLINEFILE" ]; then echo "No CMDLINE options file found. Using currently running cmdline options from /proc/cmdline"; cat /proc/cmdline > "$CMDLINEFILE"; else echo "cmdline.conf already installed in $WORKING_DIR"; fi
 echo "Make sure to update the installed mortar.env with your TPM version and ensure all the paths are correct."
+
+
+# TPM VERSION SPECIFIC PACKAGES.
+
+## Debian
+# tpm2-tools clevis-tpm2 clevis-luks
--- 4-prepluks.sh
+++ 4-prepluks.sh
@@ -1,19 +1,34 @@
-source ./linux-bitlocker.env
+MORTAR_FILE="/etc/mortar/mortar.env"
+source "$MORTAR_FILE"
 echo "Testing if secure boot is on and working."
 od --address-radix=n --format=u1 /sys/firmware/efi/efivars/SecureBoot-*
 read -p  "ENTER to continue only if the last number is a \"1\" and you are sure the TPM registers are as you want them." asdf
 echo "Wiping any existing metadata in the luks keyslot."
-luksmeta wipe -d /dev/nvme0n1p2 -s 1 #Wipe metadata from keyslot if present. 
+
+luksmeta wipe -d "$CRYPTDEV" -s "$SLOT" #Wipe metadata from keyslot if present. 
 echo "Wiping any old luks key in the keyslot. (You'll need to enter a password.)"
-cryptsetup luksKillSlot /dev/nvme0n1p2 1
+cryptsetup luksKillSlot "$CRYPTDEV" "$SLOT"
 echo "Generating clevis key, adding it to the luks slot, and mapping it to the TPM PCRs."
-clevis-luks-bind -d /dev/nvme0n1p2 -s 1 tpm2 '{"pcr_bank":"sha256","pcr_ids":"1,7"}'
-echo "Adding new sha256 of the luks header to the initramfs validation script."
+clevis-luks-bind -d "$CRYPTDEV" -s "$SLOT" tpm2 "{""pcr_bank":"sha256","pcr_ids":"$BINDPCR""}"
+echo "Adding new sha256 of the luks header to the mortar env file."
 if [ -f "$HEADERFILE" ]; then rm "$HEADERFILE"; fi
 cryptsetup luksHeaderBackup "$CRYPTDEV" --header-backup-file "$HEADERFILE"
 HEADERSHA256=$(sha256sum "$HEADERFILE" | cut -f1 -d' ')
-sed -i "s/HEADERSHA256=.*/HEADERSHA256=$HEADERSHA256/" ./linux-bitlocker.env
+sed -i -e "/HEADERSHA256=/{s//HEADERSHA256=$HEADERSHA256/;:a" -e '$!N;$!ba' -e '}' "$MORTAR_FILE"
 if [ -f "$HEADERFILE" ]; then rm "$HEADERFILE"; fi
+# Get slot uuid and write to MORTAR_FILE. 
+CURSLOTUUID=$(luksmeta show -d  "$CRYPTDEV" -s "$SLOT")
+if [ -z "$SLOTUUID" ]; then 
+	SLOTUUID="$CURSLOTUUID"
+	sed -i -e "/SLOTUUID=/{s//SLOTUUID=$SLOTUUID/;:a" -e '$!N;$!ba' -e '}' "$MORTAR_FILE"
+	echo "Updated SLOTUUID in $MORTAR_FILE"
+else
+	echo "Looks like SLOTUUID was set in the env file. Verify it is still correct with: luksmeta show -d $CRYPTDEV"
+	if [ "$SLOTUUID" == "$CURSLOTUUID" ]; then echo "[ MATCHES ]"; else echo "[ DIFFERS ]"; fi
+	echo "ENV file SLOTUUID: $SLOTUUID"
+	echo "Current SLOTUUID:  $CURSLOTUUID"
+fi
+
 
 # REMOVE FROM THIS STEP:
 # sed -i "s/HEADERSHA256=.*/HEADERSHA256=$HEADERSHA256/" /etc/initcpio/hooks/clevis
