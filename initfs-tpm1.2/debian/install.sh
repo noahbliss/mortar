10082bc1d78738f601d2477cd3e6bc3194e90c7e
Noah <noah@superuser.sh>
2020-01-15 18:11:21 -0500

    Debian update hook added.

--- initfs-tpm1.2/debian/install.sh
+++ initfs-tpm1.2/debian/install.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 # Noah Bliss
-cp -r initramfs-tools /etc/
-source /etc/mortar/mortar.env
+
+# Install the kernel upgrade hook for generation and signing of the efi.
+cp -r kernel /etc/
 
 # Install the initramfs script and update hook. 
+cp -r initramfs-tools /etc/
 INITRAMFSSCRIPTFILE='/etc/initramfs-tools/scripts/local-top/mortar'
-
+source /etc/mortar/mortar.env
 sed -i -e "/^CRYPTDEV=.*/{s##CRYPTDEV=\"$CRYPTDEV\"#;:a" -e '$!N;$!b' -e '}' "$INITRAMFSSCRIPTFILE"
 sed -i -e "/^CRYPTNAME=.*/{s//CRYPTNAME=$CRYPTNAME/;:a" -e '$!N;$!b' -e '}' "$INITRAMFSSCRIPTFILE"
 sed -i -e "/^SLOT=.*/{s//SLOT=$SLOT/;:a" -e '$!N;$!b' -e '}' "$INITRAMFSSCRIPTFILE"
@@ -13,10 +15,4 @@ sed -i -e "/^TPMINDEX=.*/{s//TPMINDEX=$TPMINDEX/;:a" -e '$!N;$!b' -e '}' "$INITR
 sed -i -e "/^HEADERSHA256=.*/{s//HEADERSHA256=$HEADERSHA256/;:a" -e '$!N;$!b' -e '}' "$INITRAMFSSCRIPTFILE"
 
 echo "Installed. You'll need to run \`update-initramfs -u\` then generate and sign the efi file."
-
-# Install the kernel upgrade hook to regenerate and sign the efi.
-cp 
-
-echo stuff
-
 
