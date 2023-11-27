3ceb910a07bd356cb012f67ba2d992b8b8aab7b7
Noah Bliss <noah@superuser.sh>
2023-11-27 03:14:53 +0000

    added missing gawk prereq
    resolves https://github.com/noahbliss/mortar/issues/30

--- res/debian/prereqs.sh
+++ res/debian/prereqs.sh
@@ -3,7 +3,8 @@ apt-get install \
         binutils \
         efitools \
         systemd-boot-efi \
-        uuid-runtime
+        uuid-runtime \
+        gawk
 
 echo "Installed Debian dependencies."
 echo "If you have a TPM 1.2 module you also need to run:"
