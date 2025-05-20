2f0bc67c790067eb235e896243adc749772cc8ec
Chris Caputo <ccaputo@alt.net>
2025-05-20 21:00:48 +0000

    Generalize FabianSchurig's "[Debian/Proxmox | TPM2] Add multiple LUKS devices."

--- 4-register-additional-luks-device.sh
+++ 4-register-additional-luks-device.sh
@@ -6,7 +6,6 @@ NEW_CRYPTDEV=$1
 NEW_CRYPTNAME=$2
 MORTAR_DIR="/etc/mortar"
 MORTAR_FILE="$MORTAR_DIR/mortar-$NEW_CRYPTNAME.env"
-CRYPTTAB_FILE="/etc/crypttab"
 FSTAB_FILE="/etc/fstab"
 INITIAL_MORTAR_FILE="$MORTAR_DIR/mortar.env"
 MOUNT_POINT="/mnt/$NEW_CRYPTNAME"
@@ -24,16 +23,6 @@ if [ -z "$UUID" ]; then
     exit 1
 fi
 NEW_CRYPTDEV="UUID=$UUID"
-
-# Check if the device is already in crypttab
-if grep -q "$NEW_CRYPTDEV" "$CRYPTTAB_FILE"; then
-    echo "$NEW_CRYPTDEV is already registered in $CRYPTTAB_FILE"
-    exit 0
-fi
-
-# Register the hard disk in crypttab
-echo "$NEW_CRYPTNAME $NEW_CRYPTDEV none luks" >> "$CRYPTTAB_FILE"
-echo "Registered $NEW_CRYPTDEV as $NEW_CRYPTNAME in $CRYPTTAB_FILE"
 
 # Source the initial mortar env file and store values in temporary variables
 source "$INITIAL_MORTAR_FILE"
@@ -69,6 +58,7 @@ echo "LUKSVER=$NEW_LUKSVER" >> "$MORTAR_FILE"
 cryptsetup luksHeaderBackup "$NEW_CRYPTDEV" --header-backup-file "/etc/mortar/$NEW_CRYPTNAME.header"
 NEW_HEADERSHA256=$(sha256sum "/etc/mortar/$NEW_CRYPTNAME.header" | cut -f1 -d' ')
 echo "HEADERSHA256=$NEW_HEADERSHA256" >> "$MORTAR_FILE"
+rm -f "/etc/mortar/$NEW_CRYPTNAME.header"
 
 # Get slot uuid if LUKS version is 1
 if [ "$NEW_LUKSVER" == "1" ]; then
@@ -77,26 +67,3 @@ if [ "$NEW_LUKSVER" == "1" ]; then
 fi
 
 echo "Created $MORTAR_FILE with necessary environment variables."
-
-# Remove existing mounts for the disk in fstab
-sed -i "\|$NEW_CRYPTNAME|d" "$FSTAB_FILE"
-
-# Create mount point and add to fstab
-mkdir -p "$MOUNT_POINT"
-echo "/dev/mapper/$NEW_CRYPTNAME $MOUNT_POINT ext4 defaults 0 2" >> "$FSTAB_FILE"
-echo "Added $NEW_CRYPTNAME to $FSTAB_FILE"
-
-# Open the LUKS device
-cryptsetup luksOpen "$NEW_CRYPTDEV" "$NEW_CRYPTNAME"
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to open LUKS device $NEW_CRYPTDEV"
-    exit 1
-fi
-
-# Mount the device
-mount "/dev/mapper/$NEW_CRYPTNAME" "$MOUNT_POINT"
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to mount $NEW_CRYPTNAME at $MOUNT_POINT"
-    exit 1
-fi
-echo "Mounted $NEW_CRYPTNAME at $MOUNT_POINT"
